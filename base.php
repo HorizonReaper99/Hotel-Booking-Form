cc98ae2920d449a378c267d5e2cf885626402b60
Natheem Scott <natheemscott99@gmail.com>
2019-05-06 10:10:35 +0300

    I design the Hotel Booking Form this way so other users can see how I code

---
+++ base.php
@@ -0,0 +1,9 @@
+<?php
+
+        //login creds
+
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', 'root');
+    define('DB_NAME',  'ROOT');
+    ?>
---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+
+        //include creds
+        require_once 'base.php';
+
+        //establish connection
+        $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+        //is connection successful
+        if ($conn->connect_error){
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+    ?>
